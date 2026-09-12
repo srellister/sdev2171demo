3f7d44e851ca0abc6ee4cf0bea9e291455a8e10b
relliott13nait <relliott13@nait.ca>
2026-09-11 23:04:06 -0600

    Demo of lesson 4 started

--- reactNativeDemo/src/app/index.tsx
+++ reactNativeDemo/src/app/index.tsx
@@ -1,34 +1,50 @@
 import { Text, View, StyleSheet, Image, TextInput, ScrollView } from "react-native";
 import {Stack} from 'expo-router';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const imageURL = "https://www.mindyourlogic.com/static/blogs/how-many-animals-in-this-image-1.webp";
 
 
   return (
-    <View style={styles.container}>
-      <Stack.Screen options={{headerShown: false}}/>
-      <ScrollView>
-      <Text style={styles.title}>How Many Animals Are in This Image?</Text>
-      {/* <Text style={styles.banner}>HOW MANY ANIMALS IN THIS IMAGE?</Text> */}
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={{ 
+          height: 300,
+          width: 300,
+          backgroundColor: "blue",
+          borderRadius: 150,
+          }}/>
+        
+        {/* <View style={{ height: 100, width:100, backgroundColor: "green",}}/> */}
 
-      <Image 
-        source={{uri: imageURL}}
-        style={styles.image}
-      />
-      <Text style={styles.label}>Your answer</Text>
-      <TextInput style={styles.input} placeholder="Enter the number of animals"></TextInput>
-      </ScrollView>
-    </View>
+      </View>
+    </SafeAreaView>
+
+    // <View style={styles.container}>
+    //   <Stack.Screen options={{headerShown: false}}/>
+    //   <ScrollView>
+    //   <Text style={styles.title}>How Many Animals Are in This Image?</Text>
+    //   {/* <Text style={styles.banner}>HOW MANY ANIMALS IN THIS IMAGE?</Text> */}
+
+    //   <Image 
+    //     source={{uri: imageURL}}
+    //     style={styles.image}
+    //   />
+    //   <Text style={styles.label}>Your answer</Text>
+    //   <TextInput style={styles.input} placeholder="Enter the number of animals"></TextInput>
+    //   </ScrollView>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "grey",
+    padding: 24,
   },
   title: {
     fontSize: 24,
